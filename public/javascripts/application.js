dee31bb5a15e9144040a8b31586a92e668dc9cf2
David Rodriguez Elizalde <stoicdavid@gmail.com>
2011-02-24 02:12:22 -0600

    diseño

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,9 @@
  $(function(){
-	$('#parallax').jparallax({});
+	$("#parallax").jparallax({},
+	{xtravel:-0.05,ytravel:0},
+	{xtravel:-0.09,ytravel:0},
+	{xtravel:0.1,ytravel:0.9},
+	{xtravel:-0.09,ytravel:0}
+	);
+
  });
